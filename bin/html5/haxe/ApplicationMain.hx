b4ae56968a868e6e9d65294fa504bde8ba1b68df
Brenninho <157061485+Brenninho123@users.noreply.github.com>
2026-01-28 08:56:23 -0300

    html5 update

--- bin/html5/haxe/ApplicationMain.hx
+++ bin/html5/haxe/ApplicationMain.hx
@@ -1,70 +1,81 @@
 package;
 
-import Main;
+#if macro
+import haxe.macro.Compiler;
+import haxe.macro.Context;
+import haxe.macro.Expr;
+#end
 
 @:access(lime.app.Application)
 @:access(lime.system.System)
-
-@:dox(hide) class ApplicationMain
+@:access(openfl.display.Stage)
+@:access(openfl.events.UncaughtErrorEvents)
+@:dox(hide)
+class ApplicationMain
 {
+	#if !macro
 	public static function main()
 	{
 		lime.system.System.__registerEntryPoint("Game", create);
 
-		#if (!html5 || munit)
+		#if (js && html5)
+		#if (munit || (utest && openfl_enable_utest_legacy_mode))
+		lime.system.System.embed("Game", null, 0, 0);
+		#end
+		#else
 		create(null);
 		#end
 	}
 
-	public static function create(config:Dynamic):Void
+	public static function create(config):Void
 	{
+		var app = new openfl.display.Application();
+
 		#if !disable_preloader_assets
 		ManifestResources.init(config);
 		#end
 
-		#if !munit
-		var app = new Main();
-		app.meta.set("build", "1");
-		app.meta.set("company", "Breno");
-		app.meta.set("file", "Game");
-		app.meta.set("name", "Windows XP");
-		app.meta.set("packageName", "com.ander.windowsXP");
-		app.meta.set("version", "1.0.0");
+		app.meta["build"] = "2";
+		app.meta["company"] = "Ander";
+		app.meta["file"] = "Game";
+		app.meta["name"] = "Windows XP";
+		app.meta["packageName"] = "com.ander.windowsXP";
+		app.meta["version"] = "1.0.0";
+
+		
 
 		#if !flash
 		
-		var attributes:lime.ui.WindowAttributes =
-			{
-				allowHighDPI: true,
-				alwaysOnTop: false,
-				borderless: false,
-				// display: 0,
-				element: null,
-				frameRate: 60,
-				#if !web fullscreen: false, #end
-				height: 0,
-				hidden: #if munit true #else false #end,
-				maximized: false,
-				minimized: false,
-				parameters: {},
-				resizable: true,
-				title: "Windows XP",
-				width: 0,
-				x: null,
-				y: null,
-			};
+		var attributes:lime.ui.WindowAttributes = {
+			allowHighDPI: true,
+			alwaysOnTop: false,
+			borderless: false,
+			// display: 0,
+			element: null,
+			frameRate: 60,
+			#if !web fullscreen: false, #end
+			height: 0,
+			hidden: #if munit true #else false #end,
+			maximized: false,
+			minimized: false,
+			parameters: {},
+			resizable: true,
+			title: "Windows XP",
+			width: 0,
+			x: null,
+			y: null,
+		};
 
-		attributes.context =
-			{
-				antialiasing: 0,
-				background: 16777215,
-				colorDepth: 32,
-				depth: true,
-				hardware: true,
-				stencil: true,
-				type: null,
-				vsync: false
-			};
+		attributes.context = {
+			antialiasing: 0,
+			background: 16777215,
+			colorDepth: 32,
+			depth: true,
+			hardware: true,
+			stencil: true,
+			type: null,
+			vsync: false
+		};
 
 		if (app.window == null)
 		{
@@ -96,9 +107,18 @@ import Main;
 		app.window.context.attributes.background = 16777215;
 		app.window.frameRate = 60;
 		#end
-		#end
 
-		// preloader.create ();
+		var preloader = getPreloader();
+		app.preloader.onProgress.add (function(loaded, total)
+		{
+			@:privateAccess preloader.update(loaded, total);
+		});
+		app.preloader.onComplete.add(function()
+		{
+			@:privateAccess preloader.start();
+		});
+
+		preloader.onComplete.add(start.bind((cast app.window:openfl.display.Window).stage));
 
 		#if !disable_preloader_assets
 		for (library in ManifestResources.preloadLibraries)
@@ -114,28 +134,118 @@ import Main;
 
 		app.preloader.load();
 
-		#if !munit
-		start(app);
-		#end
-	}
-
-	public static function start(app:lime.app.Application = null):Void
-	{
-		#if !munit
-
 		var result = app.exec();
 
-		#if (sys && !ios && !nodejs && !webassembly)
+		#if (sys && !ios && !nodejs && !emscripten)
 		lime.system.System.exit(result);
-		#end
-
-		#else
-
-		new Main();
-
 		#end
 	}
 
+	public static function start(stage:openfl.display.Stage):Void
+	{
+		#if flash
+		ApplicationMain.getEntryPoint();
+		#else
+		if (stage.__uncaughtErrorEvents.__enabled)
+		{
+			try
+			{
+				ApplicationMain.getEntryPoint();
+
+				stage.dispatchEvent(new openfl.events.Event(openfl.events.Event.RESIZE, false, false));
+
+				if (stage.window.fullscreen)
+				{
+					stage.dispatchEvent(new openfl.events.FullScreenEvent(openfl.events.FullScreenEvent.FULL_SCREEN, false, false, true, true));
+				}
+			}
+			catch (e:Dynamic)
+			{
+				#if !display
+				stage.__handleError(e);
+				#end
+			}
+		}
+		else
+		{
+			ApplicationMain.getEntryPoint();
+
+			stage.dispatchEvent(new openfl.events.Event(openfl.events.Event.RESIZE, false, false));
+
+			if (stage.window.fullscreen)
+			{
+				stage.dispatchEvent(new openfl.events.FullScreenEvent(openfl.events.FullScreenEvent.FULL_SCREEN, false, false, true, true));
+			}
+		}
+		#end
+	}
+	#end
+
+	macro public static function getEntryPoint()
+	{
+		var hasMain = false;
+
+		switch (Context.follow(Context.getType("Main")))
+		{
+			case TInst(t, params):
+
+				var type = t.get();
+				for (method in type.statics.get())
+				{
+					if (method.name == "main")
+					{
+						hasMain = true;
+						break;
+					}
+				}
+
+				if (hasMain)
+				{
+					return Context.parse("@:privateAccess Main.main()", Context.currentPos());
+				}
+				else if (type.constructor != null)
+				{
+					return macro
+					{
+						var current = stage.getChildAt (0);
+
+						if (current == null || !(current is openfl.display.DisplayObjectContainer))
+						{
+							current = new openfl.display.MovieClip();
+							stage.addChild(current);
+						}
+
+						//this define is for internal use only
+						//note: it may be removed abruptly in the future
+						#if !no_openfl_entry_point
+						new DocumentClass(cast current);
+						#end
+					};
+				}
+				else
+				{
+					Context.fatalError("Main class \"Main\" has neither a static main nor a constructor.", Context.currentPos());
+				}
+
+			default:
+
+				Context.fatalError("Main class \"Main\" isn't a class.", Context.currentPos());
+		}
+
+		return null;
+	}
+
+	macro public static function getPreloader()
+	{
+		
+		return macro
+		{
+			new openfl.display.Preloader(new openfl.display.Preloader.DefaultPreloader());
+		};
+		
+	}
+
+	#if !macro
 	@:noCompletion @:dox(hide) public static function __init__()
 	{
 		var init = lime.app.Application;
@@ -143,33 +253,32 @@ import Main;
 		#if neko
 		// Copy from https://github.com/HaxeFoundation/haxe/blob/development/std/neko/_std/Sys.hx#L164
 		// since Sys.programPath () isn't available in __init__
-		var sys_program_path =
+		var sys_program_path = {
+			var m = neko.vm.Module.local().name;
+			try
 			{
-				var m = neko.vm.Module.local().name;
-				try
+				sys.FileSystem.fullPath(m);
+			}
+			catch (e:Dynamic)
+			{
+				// maybe the neko module name was supplied without .n extension...
+				if (!StringTools.endsWith(m, ".n"))
 				{
-					sys.FileSystem.fullPath(m);
-				}
-				catch (e:Dynamic)
-				{
-					// maybe the neko module name was supplied without .n extension...
-					if (!StringTools.endsWith(m, ".n"))
+					try
 					{
-						try
-						{
-							sys.FileSystem.fullPath(m + ".n");
-						}
-						catch (e:Dynamic)
-						{
-							m;
-						}
+						sys.FileSystem.fullPath(m + ".n");
 					}
-					else
+					catch (e:Dynamic)
 					{
 						m;
 					}
 				}
-			};
+				else
+				{
+					m;
+				}
+			}
+		};
 
 		var loader = new neko.vm.Loader(untyped $loader);
 		loader.addPath(haxe.io.Path.directory(#if (haxe_ver >= 3.3) sys_program_path #else Sys.executablePath() #end));
@@ -177,4 +286,49 @@ import Main;
 		loader.addPath("@executable_path/");
 		#end
 	}
+	#end
 }
+
+#if !macro
+@:build(DocumentClass.build())
+@:keep @:dox(hide) class DocumentClass extends Main {}
+#else
+class DocumentClass
+{
+	macro public static function build():Array<Field>
+	{
+		var classType = Context.getLocalClass().get();
+		var searchTypes = classType;
+
+		while (searchTypes != null)
+		{
+			if (searchTypes.module == "openfl.display.DisplayObject" || searchTypes.module == "flash.display.DisplayObject")
+			{
+				var fields = Context.getBuildFields();
+
+				var method = macro
+				{
+					current.addChild(this);
+					super();
+					dispatchEvent(new openfl.events.Event(openfl.events.Event.ADDED_TO_STAGE, false, false));
+				}
+
+				fields.push({ name: "new", access: [ APublic ], kind: FFun({ args: [ { name: "current", opt: false, type: macro :openfl.display.DisplayObjectContainer, value: null } ], expr: method, params: [], ret: macro :Void }), pos: Context.currentPos() });
+
+				return fields;
+			}
+
+			if (searchTypes.superClass != null)
+			{
+				searchTypes = searchTypes.superClass.t.get();
+			}
+			else
+			{
+				searchTypes = null;
+			}
+		}
+
+		return null;
+	}
+}
+#end
